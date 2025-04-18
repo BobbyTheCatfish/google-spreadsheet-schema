d135ff2227b7427fb49ae06aad990eafb9ebfcb7
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2025-04-18 14:46:30 -0400

    utilset schema

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import SchemaFunction from "./schema-function"
 import ObjectSchema, { Schema as ObjectSchemaBuilder } from "./schema-object"
+import SetSchema from "./schema-set"
 import { Filter, Mapper } from "./utils"
-export { SchemaFunction, ObjectSchema, Filter, Mapper, ObjectSchemaBuilder }
+export { SchemaFunction, ObjectSchema, SetSchema, Filter, Mapper, ObjectSchemaBuilder }
---
+++ src/schema-set.ts
@@ -0,0 +1,29 @@
+import { GoogleSpreadsheetRow, GoogleSpreadsheetWorksheet } from "google-spreadsheet";
+import { DefaultType, Filter, TypeMap, valueMapper } from "./utils";
+import UtilSet from "./UtilSet";
+
+export default class SetSchema<T extends keyof TypeMap = "string"> extends UtilSet<TypeMap[T]> {
+    rows: GoogleSpreadsheetRow[]
+    key: string
+    type: keyof TypeMap
+    constructor(key: string, type?: DefaultType<T>) {
+        super();
+        this.key = key;
+        this.type = type ?? "string"
+        this.rows = []
+    }
+
+    async load(sheet: GoogleSpreadsheetWorksheet, filter: Filter = () => true, useExistingData = false) {
+        if (!useExistingData || sheet.rowCount === 0) {
+            this.rows = await sheet.getRows();
+        }
+        this.clear();
+        for (const row of this.rows) {
+            const key = row.get(this.key)
+            if (key && filter(row)) {
+                this.add(valueMapper(row, this.type) as any)
+            }
+        }
+    }
+}
+
